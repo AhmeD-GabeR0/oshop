d895227f83f0d8ea9f2c8c782c1d73b4f05498b8
gaber0 <ahmedgaber@speero.net>
2018-01-04 17:20:43 +0200

    :DEVELOP: Add app structure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAI2k959dt_Cshggp4SfzWR6gthiyt6Yvo',
+    authDomain: 'oshop-de510.firebaseapp.com',
+    databaseURL: 'https://oshop-de510.firebaseio.com',
+    projectId: 'oshop-de510',
+    storageBucket: '',
+    messagingSenderId: '1018610751006'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAI2k959dt_Cshggp4SfzWR6gthiyt6Yvo',
+    authDomain: 'oshop-de510.firebaseapp.com',
+    databaseURL: 'https://oshop-de510.firebaseio.com',
+    projectId: 'oshop-de510',
+    storageBucket: '',
+    messagingSenderId: '1018610751006'
+  }
 };
